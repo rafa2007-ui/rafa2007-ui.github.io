e5dab0ddb1fe616530aa553c78af42f5d36358c1
rafa2007-ui <rafaktm2007@gmail.com>
2026-05-15 11:14:27 +0200

    Delete JavaScript directory

--- JavaScript/Ejercicio9/javascript.js
+++
@@ -1,7 +0,0 @@
-function modulo(){
-
-let nombre = document.getElementById("nombre").value
-let numero = nombre.length;
-
-document.getElementById("resultado").innerHTML = "La cantidad de caracteres es " + numero
-}
